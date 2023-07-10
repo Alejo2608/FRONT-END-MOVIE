874783c17af95d21f6608c18756f2131f38e5f29
Alejo2608 <bricenovalecillose@uvm.edu.ve>
2023-07-09 22:04:57 -0400

    Front casi terminado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.scss'
+import './index.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import MovieDetail from './components/MovieDetail/MovieDetail'
+import Footer from './components/Footer/Footer'
+import PageNotFound from './components/PageNotFound/PageNotFound'
 import Login from './components/Login/Login'
+import Signup from './components/Register/Register'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     <Login/>
-    </>
+    <div className="app">
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path='/home' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/regi' element={<Signup />} />
+            <Route path='/movie/:imdbID' element={<MovieDetail />} />
+            <Route element={<PageNotFound />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </div>
   )
 }
 
